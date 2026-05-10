62c60471004831f69ccdef6fdf99238457fc685f
Hassan <hassangaming111@gmail.com>
2026-05-10 14:36:33 +0500

    region change

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 const LOCAL_API = "http://localhost:8000/api";
-const PROD_API = "https://aegis-api-762161152188.us-central1.run.app/api";
+const PROD_API = "https://aegis-api-762161152188.asia-south1.run.app/api";
 
 export const API_URL = isLocal ? LOCAL_API : PROD_API;
